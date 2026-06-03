9fc29401e5f6832b4e392ed3bb17cfb0acdd9f9f
ykrank <ykrank@gmail.com>
2026-06-03 20:25:28 +0800

    Migrate library base layouts to view binding

--- library/src/main/java/com/github/ykrank/androidtools/ui/adapter/delegate/FooterAdapterDelegate.kt
+++ library/src/main/java/com/github/ykrank/androidtools/ui/adapter/delegate/FooterAdapterDelegate.kt
@@ -1,10 +1,8 @@
 package com.github.ykrank.androidtools.ui.adapter.delegate
 
 import android.content.Context
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
-import com.github.ykrank.androidtools.R
 import com.github.ykrank.androidtools.databinding.ItemFooterBinding
 import com.github.ykrank.androidtools.ui.adapter.delegate.item.FooterItem
 
@@ -13,7 +11,7 @@ class FooterAdapterDelegate(context: Context) : LibBaseAdapterDelegate<FooterIte
         FooterAdapterDelegate.FooterViewHolder>(context, FooterItem::class.java) {
 
     public override fun onCreateViewHolder(parent: ViewGroup): androidx.recyclerview.widget.RecyclerView.ViewHolder {
-        return FooterViewHolder(DataBindingUtil.inflate(mLayoutInflater, R.layout.item_footer, parent, false))
+        return FooterViewHolder(ItemFooterBinding.inflate(mLayoutInflater, parent, false))
     }
 
     override fun onBindViewHolderData(t: FooterItem, position: Int, holder: FooterViewHolder, payloads: List<Any>) {
--- library/src/main/java/com/github/ykrank/androidtools/ui/adapter/delegate/ProgressAdapterDelegate.kt
+++ library/src/main/java/com/github/ykrank/androidtools/ui/adapter/delegate/ProgressAdapterDelegate.kt
@@ -1,11 +1,9 @@
 package com.github.ykrank.androidtools.ui.adapter.delegate
 
 import android.content.Context
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import android.view.ViewGroup
 
-import com.github.ykrank.androidtools.R
 import com.github.ykrank.androidtools.databinding.ItemProgressBinding
 import com.github.ykrank.androidtools.ui.adapter.delegate.item.ProgressItem
 
@@ -14,7 +12,7 @@ class ProgressAdapterDelegate(context: Context) : LibBaseAdapterDelegate<Progres
         ProgressAdapterDelegate.ProgressViewHolder>(context, ProgressItem::class.java) {
 
     public override fun onCreateViewHolder(parent: ViewGroup): androidx.recyclerview.widget.RecyclerView.ViewHolder {
-        return ProgressViewHolder(DataBindingUtil.inflate(mLayoutInflater, R.layout.item_progress, parent, false))
+        return ProgressViewHolder(ItemProgressBinding.inflate(mLayoutInflater, parent, false))
     }
 
     override fun onBindViewHolderData(t: ProgressItem, position: Int, holder: ProgressViewHolder, payloads: List<Any>) {
